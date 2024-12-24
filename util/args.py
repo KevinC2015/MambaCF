00eab2c8a52a35a2e0a854a4bc80e4b7d8513a05
KevinCHY <109566931+KevinC2015@users.noreply.github.com>
2024-12-24 12:11:02 -0800

    Update args.py

--- util/args.py
+++ util/args.py
@@ -3,7 +3,7 @@ def get_params():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default="LightGCN")
-    parser.add_argument('--num_neg', type=int, default=10)
+    parser.add_argument('--num_neg', type=int, default=1)
     parser.add_argument('--margin', type=float, default=1.0)
     parser.add_argument('--gamma', type=float, default=1.0)
     parser.add_argument('--loss', type=str, default='bpr')
