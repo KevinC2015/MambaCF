d03ac21576768d2b2af4aba4d1fd03dd030f4913
KevinCHY <109566931+KevinC2015@users.noreply.github.com>
2024-12-24 11:40:00 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 main.py --dataset=ml-1M --trainset=./dataset/ml-1M/train.txt --testset=./dataset/ml-1M/test.txt --model=LightGCN --num_neg=64 --margin=10.0 --loss=simce
+python3 main.py --dataset=pinterest --trainset=./dataset/pinterest/train.txt --testset=./dataset/pinterest/test.txt --model=MambaCF --loss=bpr
